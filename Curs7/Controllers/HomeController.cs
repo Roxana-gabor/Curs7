dcc81567562183faa58ac016c8481bc9ce02db5a
Roxana Gabor <Roxana Gabor>
2016-11-28 16:32:16 +0200

    Curs 7 + Tema

---
+++ Curs7/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Curs7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ActionName("about")]
+        public ActionResult SomeMethodName()
+        {
+            return View("");
+
+        }
+        [NonAction]
+        public double Calc()
+        {
+            return 1 + 2 + 3 + 4 + 5;
+        }
+        [HttpGet]
+        public ActionResult VerbsTest()
+        {
+            return View();
+        }
+
+        //[ActionName("Authorize")]
+        [Authorize(Roles ="Admin")]
+        public ActionResult ThisMethodNeedsAuthorization()
+        {
+            return Content("Hello!");
+        }
+    }
+}
